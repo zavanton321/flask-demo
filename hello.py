e861a19f379e6cd3e343453fe5b9fbc8d7b673a5
Anton Zavyalov <zavanton123@gmail.com>
2019-12-07 19:54:36 +0300

    form ok

--- hello.py
+++ hello.py
@@ -1,14 +1,29 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+
+from wtforms.validators import Length, DataRequired
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "MY_KEY"
 bootstrap = Bootstrap(app)
 
 
-@app.route("/", methods=['GET', 'POST'])
+class NameForm(Form):
+    name = StringField("Name: ", validators=[DataRequired(), Length(1, 16)])
+    submit = SubmitField("Submit")
+
+
+@app.route("/", methods=["GET", "POST"])
 def index():
     name = None
-    return render_template("index.html", name=name)
+    form = NameForm()
+
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ""
+    return render_template("index.html", name=name, form=form)
 
 
 if __name__ == "__main__":
