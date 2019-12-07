e56daf0764182c63e6ec2a54a4a354b43341d9fa
Anton Zavyalov <zavanton123@gmail.com>
2019-12-07 20:54:20 +0300

    upload image

--- hello.py
+++ hello.py
@@ -1,29 +1,36 @@
+import imghdr
+import os
+
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_wtf import Form
-from wtforms import StringField, SubmitField
-
-from wtforms.validators import Length, DataRequired
+from wtforms import SubmitField, FileField, ValidationError
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "MY_KEY"
 bootstrap = Bootstrap(app)
 
 
-class NameForm(Form):
-    name = StringField("Name: ", validators=[DataRequired(), Length(1, 16)])
+class UploadForm(Form):
+    image_file = FileField("Image File")
     submit = SubmitField("Submit")
+
+    def validate_image_file(self, field):
+        if field.data.filename[-4:].lower() != ".jpg":
+            raise ValidationError("Invalid file extension!")
+        if imghdr.what(field.data) != "jpeg":
+            raise ValidationError("Invalid image format!")
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    name = None
-    form = NameForm()
+    image = None
+    form = UploadForm()
 
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ""
-    return render_template("index.html", name=name, form=form)
+        image = "uploads/" + form.image_file.data.filename
+        form.image_file.data.save(os.path.join(app.static_folder, image))
+    return render_template("index.html", form=form, image=image)
 
 
 if __name__ == "__main__":
