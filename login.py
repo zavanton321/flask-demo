85fd689f8dd11f921f1c7327c450217bd60ff1c8
Anton Zavyalov <zavanton123@gmail.com>
2019-12-08 15:11:32 +0300

    simple login

--- login.py
+++ login.py
@@ -1,31 +1,24 @@
 from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap
-from flask_login import LoginManager, UserMixin, login_required, logout_user, login_user
+from flask_login import UserMixin, LoginManager, login_user, login_required, logout_user
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import Form
 from werkzeug.security import generate_password_hash, check_password_hash
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import Length, DataRequired
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'top secret!'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.sqlite3'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config["SECRET_KEY"] = "MY_KEY"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.sqlite3"
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 lm = LoginManager(app)
-lm.login_view = 'login'
-
-
-class LoginForm(Form):
-    username = StringField('Username', validators=[DataRequired(), Length(1, 16)])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember me')
-    submit = SubmitField('Submit')
+lm.login_view = "login"
 
 
 class User(UserMixin, db.Model):
-    __tablename__ = 'users'
+    __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(16), index=True, unique=True)
     password_hash = db.Column(db.String(64))
@@ -44,14 +37,21 @@ class User(UserMixin, db.Model):
         db.session.commit()
         return user
 
-    def __repr__(self):
-        return '<User {0}>'.format(self.username)
+
+class LoginForm(Form):
+    username = StringField("Username:", validators=[DataRequired(), Length(1, 16)])
+    password = PasswordField("Password:", validators=[DataRequired()])
+    remember_me = BooleanField("Remember me")
+    submit = SubmitField("Login")
 
 
 @app.before_first_request
-def create_user():
-    if User.query.filter_by(username='john').first() is None:
-        User.register('john', 'cat')
+def setup_data():
+    db.create_all()
+    user = User.query.filter_by(username="john").first()
+    if user is None:
+        user = User.register("john", "cat")
+    return user
 
 
 @lm.user_loader
@@ -59,35 +59,31 @@ def load_user(id):
     return User.query.get(int(id))
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user is None or not user.verify_password(form.password.data):
-            return redirect(url_for('login', **request.args))
-        login_user(user, form.remember_me.data)
-        return redirect(request.args.get('next') or url_for('index'))
-    return render_template('login.html', form=form)
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(username=login_form.username.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user, login_form.remember_me.data)
+            return redirect(request.args.get("next") or url_for("index"))
+        else:
+            return redirect(url_for("login", **request.args))
+
+    return render_template("login.html", form=login_form)
 
 
-@app.route('/logout')
+@app.route("/logout")
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('index'))
+    return redirect(url_for("index"))
 
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-
-@app.route('/protected')
-@login_required
-def protected():
-    return render_template('protected.html')
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
