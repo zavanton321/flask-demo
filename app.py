add6454d0e8987036c6528f935633cbc61c88ed0
Anton Zavyalov <zavanton123@gmail.com>
2019-12-08 09:57:32 +0300

    app.before_request

--- app.py
+++ app.py
@@ -1,16 +1,30 @@
-from flask import Flask, render_template, session
+from datetime import datetime
+
+from flask import Flask, render_template, session, g
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 app.config["SECRET_KEY"] = "SOME_KEY"
 
 
-@app.route("/")
-def home():
+@app.before_request
+def before():
     if "count" not in session:
         session["count"] = 1
     else:
         session["count"] += 1
-    return render_template("main.html", count=session["count"])
+    g.when = datetime.now().strftime("%H:%M:%S")
+
+
+@app.route("/")
+def home():
+    return render_template("main.html", count=session["count"], when=g.when)
+
+
+@app.route("/other")
+def other():
+    return render_template("other.html", count=session["count"], when=g.when)
 
 
 if __name__ == "__main__":
