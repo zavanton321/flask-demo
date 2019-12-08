cba66601b8953e699b54bac8d968925333847b43
Anton Zavyalov <zavanton123@gmail.com>
2019-12-08 11:20:32 +0300

    init db

--- app.py
+++ app.py
@@ -1,33 +1,42 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Length
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "MY_KEY"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///info.sqlite3"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
 
 
-@app.route("/")
-def weather():
-    months = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
-              'August', 'September', 'October', 'November', 'December']
-    data = {
-        'January': {'min': 38, 'max': 47, 'rain': 6.14},
-        'February': {'min': 38, 'max': 51, 'rain': 4.79},
-        'March': {'min': 41, 'max': 56, 'rain': 4.5},
-        'April': {'min': 44, 'max': 61, 'rain': 3.4},
-        'May': {'min': 49, 'max': 67, 'rain': 2.55},
-        'June': {'min': 53, 'max': 73, 'rain': 1.69},
-        'July': {'min': 57, 'max': 80, 'rain': 0.59},
-        'August': {'min': 58, 'max': 80, 'rain': 0.71},
-        'September': {'min': 54, 'max': 75, 'rain': 1.54},
-        'October': {'min': 48, 'max': 63, 'rain': 3.42},
-        'November': {'min': 41, 'max': 52, 'rain': 6.74},
-        'December': {'min': 36, 'max': 45, 'rain': 6.94}
-    }
-    city = "Moscow"
-    highlight = {"min": 40, "max": 80, "rain": 5}
-    return render_template("weather.html",
-                           months=months,
-                           city=city,
-                           data=data,
-                           highlight=highlight)
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(10), index=True)
+
+    def __repr__(self):
+        return "<User {}>".format(self.name)
+
+
+class NameForm(Form):
+    name = StringField("Name: ", validators=[DataRequired(), Length(1, 10)])
+    submit = SubmitField("Register")
+
+
+@app.route("/", methods=['GET', 'POST'])
+def home():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ""
+        db.session.add(User(name=name))
+        db.session.commit()
+    return render_template("home.html", form=form, name=name)
 
 
 if __name__ == "__main__":
